bd5f5169c2c29443632cecd4e6004514316e8c48
Maarten Claes <maartencls@gmail.com>
2015-11-28 13:22:36 +0100

    Update to latest version of nomad, with better support for postgres

--- migrations/000_setup_migrations.go
+++ migrations/000_setup_migrations.go
@@ -13,21 +13,10 @@ import (
 
 var Migrations *nomad.List
 
-// Context will be available to each migration and should be used to provide
-// access to the database
-type Context struct {
-	DB *sql.DB
-}
-
-// This struct will be used as an argument to each migrations Up/Down func.
-// You can use this to get access to your database.
-var context = &Context{}
-
 func init() {
 	db, err := sql.Open("postgres", config.DatabaseUrl())
 	if err != nil {
 		log.Fatal(err)
 	}
-	context.DB = db
-	Migrations = nomad.NewList(pg.NewVersionStore(db), context)
+	Migrations = pg.NewList(db)
 }
--- migrations/2015-11-22_18:07:05_create_cards.go
+++ migrations/2015-11-22_18:07:05_create_cards.go
@@ -2,25 +2,26 @@ package migrations
 
 import (
 	"github.com/mcls/nomad"
+	"github.com/mcls/nomad/pg"
 )
 
 func init() {
 	migration := &nomad.Migration{
 		Version: "2015-11-22_18:07:05",
 		Up: func(ctx interface{}) error {
-			c := ctx.(*Context)
-			_, err := c.DB.Exec(`
+			c := ctx.(*pg.Context)
+			_, err := c.Tx.Exec(`
 			CREATE TABLE cards (
 				id serial PRIMARY KEY,
 				front text NOT NULL CHECK(length(front) < 4000),
-				back text NOT NULL CHECK(length(front) < 4000),
+				back text NOT NULL CHECK(length(back) < 4000),
 				created_at timestamp with time zone DEFAULT(current_timestamp)
 			)`)
 			return err
 		},
 		Down: func(ctx interface{}) error {
-			c := ctx.(*Context)
-			_, err := c.DB.Exec("DROP TABLE cards")
+			c := ctx.(*pg.Context)
+			_, err := c.Tx.Exec("DROP TABLE cards")
 			return err
 		},
 	}
