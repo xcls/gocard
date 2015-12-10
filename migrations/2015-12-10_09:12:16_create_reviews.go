248d7bd45c9c1bc28ed417b085c070c7370200e5
Maarten Claes <maartencls@gmail.com>
2015-12-10 09:39:07 +0100

    Created reviews table

---
+++ migrations/2015-12-10_09:12:16_create_reviews.go
@@ -0,0 +1,34 @@
+package migrations
+
+import (
+	"fmt"
+
+	"github.com/mcls/nomad"
+	"github.com/mcls/nomad/pg"
+)
+
+func init() {
+	migration := &nomad.Migration{
+		Version: "2015-12-10_09:12:16",
+		Up: func(ctx interface{}) error {
+			c := ctx.(*pg.Context)
+			_, err := c.Tx.Exec(`
+			CREATE TABLE reviews (
+				id serial PRIMARY KEY,
+				ease_factor numeric(6, 4)	NOT NULL CHECK(ease_factor >= 1.3),
+				interval	integer			NOT NULL CHECK(interval >= 0),
+				due_on		timestamp with time zone DEFAULT(current_timestamp),
+				card_id		integer			NOT NULL REFERENCES cards ON DELETE RESTRICT,
+				user_id		integer			NOT NULL REFERENCES users ON DELETE CASCADE,
+				created_at timestamp with time zone DEFAULT(current_timestamp)
+			)`)
+			return err
+		},
+		Down: func(ctx interface{}) error {
+			c := ctx.(*pg.Context)
+			_, err := c.Tx.Exec(`DROP TABLE reviews`)
+			return err
+		},
+	}
+	Migrations.Add(migration)
+}
